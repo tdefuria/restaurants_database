b4970e3fd47a8c7da84e5e9fa64bfc85db3d000c
Helen Cain <helenmcain9794@gmail.com>
2026-03-28 16:08:15 -0400

    add_census includes tracts, block groups, blocks

--- add_census.py
+++ add_census.py
@@ -1,18 +1,20 @@
 import pymysql
 import csv
 
-# csv filepath here!!!
-filepath = None
+# csv filepaths here!!!
+tracts_filepath = None
+bgs_filepath = None
+blocks_filepath = None
 
 cnx = pymysql.connect(
     host='localhost',
     user='root',
-    passwd='your_password',
+    passwd='password',
     db='food_inspections'
 )
 c = cnx.cursor()
 
-with open(filepath, 'r') as f:
+with open(tracts_filepath, 'r') as f:
     reader = csv.DictReader(f)
     for row in reader:
         c.execute("""
@@ -20,6 +22,28 @@ with open(filepath, 'r') as f:
             VALUES (%s, %s)
         """, (row['CT_ID_10'], 2010))
 
+print("Processed census tracts")
+
+with open(bgs_filepath, 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        c.execute("""
+            INSERT INTO census_block_group (block_group_id, census_year, tract_id)
+            VALUES (%s, %s, %s)
+        """, (row['BG_ID_10'], 2010, row['CT_ID_10']))
+        
+print("Processed census block groups")
+
+with open(blocks_filepath, 'r') as f:
+    reader = csv.DictReader(f)
+    for row in reader:
+        c.execute("""
+            INSERT INTO census_block (block_id, block_group_id, census_year)
+            VALUES (%s, %s, %s)
+        """, (row['Blk_ID_10'], row['BG_ID_10'], 2010))
+
+print("Processed census blocks")
+print("Done!")
+
 cnx.commit()
 cnx.close()
-print("Done!")
