d82dc7796c91da201e97cbfa5432b03cf97670c4
Helen Cain <helenmcain9794@gmail.com>
2026-03-29 16:10:57 -0400

    Wrote a helper function "call_proc" to streamline calling procedures. also added two more value cards

--- app.py
+++ app.py
@@ -7,6 +7,15 @@ import pymysql as sql
 # save value for database connection
 cnx = reactive.Value(None)
 
+# helper function for calling processes in value cards
+def call_proc(proc_name):
+    conn = cnx()
+    if conn is None:
+        return ""
+    c = conn.cursor()
+    c.callproc(f'food_inspections.{proc_name}')
+    return str(c.fetchone()[0])
+
 # title banner - top of screen
 ui.page_opts(
     title="Boston Restaurants: Inspections and Reviews", fillable=False)
@@ -54,45 +63,52 @@ with ui.navset_pill(id="selected_navset_pill"):
 
         ICONS = {"utensils": fa.icon_svg("utensils"),
                  "clipboard": fa.icon_svg("clipboard"),
-                 "yelp": fa.icon_svg("yelp")}
+                 "yelp": fa.icon_svg("yelp"),
+                 "star": fa.icon_svg("star"),
+                 "triangle-exclamation": fa.icon_svg("triangle-exclamation")
+                 }
 
         with ui.layout_columns(fill=False):
+
+            # summary value boxes: total restaurants
             with ui.value_box(showcase=ICONS["utensils"]):
                 "Total restaurants"
 
                 @render.text
                 def total_restaurants():
-                    conn = cnx()
-                    if conn is None:
-                        return ""
-                    c = conn.cursor()
-                    c.execute("SELECT COUNT(*) FROM restaurant")
-                    return str(c.fetchone()[0])
+                    return call_proc('get_restaurant_count')
 
-
+            # value box: total inspections
             with ui.value_box(showcase=ICONS["clipboard"]):
                 "Total health inspections"
 
                 @render.text
                 def total_health_inspections():
-                    conn = cnx()
-                    if conn is None:
-                        return ""
-                    c = conn.cursor()
-                    c.execute("SELECT COUNT(*) FROM inspection")
-                    return str(c.fetchone()[0])
+                    return call_proc('get_inspection_count')
 
+            # value box: total reviews
             with ui.value_box(showcase=ICONS["yelp"]):
                 "Total reviews"
 
                 @render.text
                 def total_reviews():
-                    conn = cnx()
-                    if conn is None:
-                        return ""
-                    c = conn.cursor()
-                    c.execute("SELECT COUNT(*) FROM review")
-                    return str(c.fetchone()[0])
+                    return call_proc('get_review_count')
+
+            # value box: average rating
+            with ui.value_box(showcase=ICONS["star"]):
+                "Average rating per restaurant"
+
+                @render.text
+                def avg_rating():
+                    return call_proc('get_avg_rating')
+
+            # value box: average violations per inspection
+            with ui.value_box(showcase=ICONS["triangle-exclamation"]):
+                "Average violations per inspection"
+
+                @render.text
+                def avg_violations():
+                    return call_proc('get_avg_violations_per_inspection')
 
     # Restaurant Search panel
     with ui.nav_panel("Search Restaurants"):
