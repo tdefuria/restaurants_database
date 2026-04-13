dbcb1c8d2bb34a2b41b645b2e3204a04a62a8e51
Helen Cain <helenmcain9794@gmail.com>
2026-04-13 10:42:43 -0400

    Corrected labeling on violations-by-level graph
    Also added new demo query from presentation

--- demo_queries.sql
+++ demo_queries.sql
@@ -1,7 +1,14 @@
 USE food_inspections;
 
+
+-- all dashboard elements are populated with SQL queries
 CALL get_violations_by_level;
 
+SELECT * FROM restaurant
+WHERE business_name = "Savin Scoops";
+
+
+-- main user CRUD operations performed on restaurant reviews
 SELECT * FROM app_user
 	JOIN review USING(username) 
 WHERE username = "helen.me";
